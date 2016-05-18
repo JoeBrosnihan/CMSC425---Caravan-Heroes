680fba3eacf6d8bef78a8e0bb71082ea84e6db58
joe <joe@spicyshrimp>
2016-05-18 04:53:58 -0400

    Add title screen and music

--- app/src/main/java/com/joe/proceduralgame/GameActivity.java
+++ app/src/main/java/com/joe/proceduralgame/GameActivity.java
@@ -1,25 +1,56 @@
 package com.joe.proceduralgame;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class GameActivity extends Activity {
 	
 	private GameGLView glView;
+	private boolean started = false;
+	private MediaPlayer musicPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-	    setContentView(R.layout.activity_game);
-	    GUIManager guiManager = new GUIManager(this);
-        glView = new GameGLView(this, guiManager);
-	    ((ViewGroup) getWindow().getDecorView().getRootView()).addView(glView, 0);
+	    setContentView(R.layout.title_screen);
+
+	    musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.main_theme);
+	    musicPlayer.setLooping(true);
     }
 
+	protected void onStart() {
+		super.onStart();
+		musicPlayer.start();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		musicPlayer.stop();
+	}
+
+	public void onBeginClick(View v) {
+		if (started)
+			return;
+		started = true;
+
+		musicPlayer.stop();
+
+		setContentView(R.layout.activity_game);
+		GUIManager guiManager = new GUIManager(this);
+		glView = new GameGLView(this, guiManager);
+		((ViewGroup) getWindow().getDecorView().getRootView()).addView(glView, 0);
+
+		musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.dungeon_music_1);
+		musicPlayer.setLooping(true);
+		musicPlayer.start();
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
